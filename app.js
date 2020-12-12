8e9f5d5be2352740b5e0dba4080a36bbd23cd1a2
Adrian Milsap <adrian@hirobius.com>
2020-12-12 15:42:27 -0800

    initial js experimentalsto break things :)

---
+++ app.js
@@ -0,0 +1,44 @@
+console.log("Hello, world");
+
+// here is a comment in javascript, variables and functions are in camelCase
+//you can't print into the console without a console log, the console log outputs a method to the web console 
+//variables are like containers
+
+//return values are RETURNED to us
+
+
+
+window.onload = function () {
+
+var myVariableName;
+myVariableName = "Hello, WORLD!!!";
+
+console.log(myVariableName);
+
+
+var order; //walk into starbies
+order = " black coffee "; //place order
+console.log("Your order of" + order + "is ready");
+
+console.log(myVariableName)
+
+var promptQuestion= "What is the best starter pokemon?";
+var starterPokemon = prompt(promptQuestion);
+console.log(starterPokemon);
+
+if (Response === "Squirtle") {
+    console.log("I feel it.");
+} else if (response === "Bulbasaur") {
+    alertMessage = "I mean...";
+} else {
+    alertMessage = "no.";
+}
+
+alert(alertMessage)
+
+
+var element = document.getElementById("response-output");
+console.log(element)
+
+
+}
